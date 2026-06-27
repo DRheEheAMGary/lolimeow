55271149f051fffb0dde6773107519c39fe682c7
DRheEheAM_GaryHanserV.N.C. <dream20110513Gary@163.com>
2026-06-28 07:49:36 +0800

    upd

--- page/p-home.php
+++ page/p-home.php
@@ -17,6 +17,19 @@ function boxmoe_home_fullscreen_banner_css() {
 add_action('wp_head', 'boxmoe_home_fullscreen_banner_css', 99);
 
 get_header(); 
+// 替换全局主查询为博客文章列表，避免「主页」页面本身作为文章卡片出现
+global $wp_query, $wp_the_query;
+$paged = get_query_var('paged') ?: 1;
+$wp_query = new WP_Query(array(
+    'post_type'      => 'post',
+    'posts_per_page' => get_option('posts_per_page', 10),
+    'paged'          => $paged,
+    'orderby'        => 'date',
+    'order'          => 'DESC',
+    'ignore_sticky_posts' => 0,
+));
+$wp_the_query = $wp_query;
 get_template_part('page/template/blog-list');
+wp_reset_query();
 get_sidebar();
 get_footer();
